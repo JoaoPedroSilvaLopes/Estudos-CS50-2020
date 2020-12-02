b1e2ffa28be01c2af32fa21a6089f4f8a5c61046
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-02 19:58:34 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -44,17 +44,17 @@ int main(void) //Usei o mesmo código do Mario Less, mas com algumas alteraçõe
                 system("sleep 0.5"); //delay na construção da piramide pra ficar legal KAKAKA.
                 SomadorHorizontal2 = SomadorHorizontal2 + 1; //SomadorHorizontal2 deve aumentar, imprimir hashes e ficar alinhado à esquerda.
             }
-            printf("\nDeseja escolher a altura novamente? Se sim pressione 1, se deseja sair pressione qualquer outra tecla.\n"); //Pergunta para o usuário se o mesmo quer repetir o processo.
+            printf("\nDeseja escolher a altura novamente? Se sim pressione 0, se deseja sair pressione qualquer outra tecla.\n"); //Pergunta para o usuário se o mesmo quer repetir o processo.
             scanf("%i", &Resposta); //Resposta do usuário.
             system("clear || cls");
         }
         else //Else para as alturas inválidas.
         {
-            printf("Não foi inserido uma altura válida.\nDeseja escolher a altura novamente? Se sim pressione 1, se deseja sair pressione qualquer outra tecla.\n");
+            printf("Não foi inserido uma altura válida.\nDeseja escolher a altura novamente? Se sim pressione 0, se deseja sair pressione qualquer outra tecla.\n");
             scanf("%i", &Resposta);
             system("clear || cls");
         }
     }
-    while (Resposta == 1); //Condição para que a repetição continue.
+    while (Resposta == 0); //Condição para que a repetição continue.
     return 0;
 }
