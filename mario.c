ddc09807b6286d1b77294b9d6d88a30c3bfbe2ad
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-06 00:56:49 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -9,19 +9,24 @@ int main(void)
     {
         Altura = get_int("Digite a altura da piramide entre 1 e 8.\n");
     }
-    while(Altura < 1 || Altura > 8);
+    while (Altura < 1 || Altura > 8);
 
-            for (int Coluna = 1; Coluna <= Altura; Coluna++)
+            int Y = Altura -1;
+            int X1 = 1;
+            int X2 = 1;
+            for (int Coluna = 0; Coluna < Altura; Coluna++)
             {
-                for (int Linha = (Altura - Coluna); Linha > 0; Linha--)
+                for (int Linha = 0; Linha < Y; Linha++)
                 {
                     printf(" ");
                 }
-                for (int Linha = 0; Linha < Coluna; Linha++)
+                Y--;
+                for (int Linha = 0; Linha < X1; Linha++)
                 {
                     printf("#");
                 }
-                printf("\n");
-            }
-return 0;
+                X1++;
+                printf("  ");
+    }
+    return 0;
 }
