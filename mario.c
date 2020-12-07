1850ff347ed1fef00383e49db30e096430f2c751
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-07 04:32:12 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -3,34 +3,33 @@
 //USUÁRIO: JoãoPedroSilvaLopes
 //EMAIL INSTITUCIONAL: joao.pedro.silva06@aluno.ifce.edu.br
 
-#include <stdio.h>
+#include <stdio.h> //Biblioteca padrão
 #include <cs50.h> //Biblioteca para o get_int()
 
 int main(void)
 {
-    int Altura;
-    do
+    int Altura; //Variável Altura
+    do //Do While para o usuário inserir a altura correta
     {
-        Altura = get_int("Digite a altura da piramide entre 1 e 8.\n");
+        Altura = get_int("Digite a altura da piramide entre 1 e 8.\n"); //Get_int() com a Altura
     }
-    while (Altura < 1 || Altura > 8);
+    while (Altura < 1 || Altura > 8); //Condição de repetição para o Do While aconteceça
 
-    int Y = Altura - 1;
-    int X1 = 1;
-    int X2 = 1;
-    for (int Coluna = 0; Coluna < Altura; Coluna++)
+    int Y = Altura - 1; //Variável que irá dar o tamanho da impressão dos espaços vazios
+    int X = 1; //Varável que irá dar o tamanho da impressão das hashes
+    for (int Coluna = 0; Coluna < Altura; Coluna++) //For que vai dar a Altura da piramide
     {
-      for (int Linha = 0; Linha < Y; Linha++)
-      {
-          printf(" ");
-      }
-      Y--;
-      for (int Linha = 0; Linha < X1; Linha++)
-      {
-          printf("#");
-      }
-      X1++;
-      printf("\n");
+        for (int Linha = 0; Linha < Y; Linha++) //For que vai imprimir os espaços vazios
+        {
+            printf(" "); //Prinft para imprimir espaços vazios
+        }
+        Y--; //Y deve ir diminuindo para que os espaços vazios apareçam cada vez menos
+        for (int Linha = 0; Linha < X; Linha++) //For que vai imprimir hashes
+        {
+            printf("#"); //Printf para imprimir hashes
+        }
+        X++; //X deve ir aumetando para que as hashes apareçam cada vez mais
+        printf("\n"); //Printf para pular linha
     }
     return 0;
 }
