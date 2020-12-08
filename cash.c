f39b09231dc1c765073f706486b38ea237646bd3
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-08 23:53:12 +0000

    automated commit by check50 [check50=True]

--- cash.c
+++ cash.c
@@ -9,37 +9,31 @@
 
 int main(void)
 {
-float Money;
-float M1;
-float M2;
-float M3;
-float M4;
-int QuantMoedas;
-int QuantMoedas1;
-int QuantMoedas2;
-int QuantMoedas3;
-int MoedasTotal;
-   
-   do
-   {
-      Money = get_float("Digite o saldo: ");
-      // ------------------------------
-      QuantMoedas = round((Money/0.25)*100)/100;
-      printf("%d\n\n", QuantMoedas);
-      // ------------------------------
-      QuantMoedas1 = round(((Money - (QuantMoedas*0.25))/0.10)*100)/100;
-      printf("%d\n\n", QuantMoedas1);
-      // ------------------------------
-      QuantMoedas2 = round(((Money - (QuantMoedas*0.25) - (QuantMoedas1*0.10))/0.05)*100)/100;
-      printf("%d\n\n", QuantMoedas2);
-      // ------------------------------
-      QuantMoedas3 = round(((Money - (QuantMoedas*0.25) - (QuantMoedas1*0.10) - (QuantMoedas2*0.05))/0.01)*100)/100;
-      printf("%d\n\n", QuantMoedas3);
-      // ------------------------------
-      MoedasTotal = QuantMoedas + QuantMoedas1 +QuantMoedas2 + QuantMoedas3;
-      printf("%d\n", MoedasTotal);
-   }
-   while(Money <= 0);
-    
-return 0;    
+    float Money;
+    int QuantMoedas;
+    int QuantMoedas1;
+    int QuantMoedas2;
+    int QuantMoedas3;
+    int MoedasTotal;
+    do
+    {
+        Money = get_float("Digite o saldo: ");
+        // ------------------------------
+        QuantMoedas = round((Money / 0.25) * 100) / 100;
+        printf("Será preciso %d moeda(as) de 25 centavos.\n\n", QuantMoedas);
+        // ------------------------------
+        QuantMoedas1 = round(((Money - QuantMoedas * 0.25) / 0.10) * 100) / 100;
+        printf("Será preciso %d moeda(as) de 10 centavos.\n\n", QuantMoedas1);
+        // ------------------------------
+        QuantMoedas2 = round(((Money - QuantMoedas * 0.25 - QuantMoedas1 * 0.10) / 0.05) * 100) / 100;
+        printf("Será preciso %d moeda(as) de 5 centavos.\n\n", QuantMoedas2);
+        // ------------------------------
+        QuantMoedas3 = round(((Money - QuantMoedas * 0.25 - QuantMoedas1 * 0.10 - QuantMoedas2 * 0.05) / 0.01) * 100) / 100;
+        printf("Será preciso %d moeda(as) de 1 centavo.\n\n", QuantMoedas3);
+        // ------------------------------
+        MoedasTotal = QuantMoedas + QuantMoedas1 + QuantMoedas2 + QuantMoedas3;
+        printf("%d\n", MoedasTotal);
+    }
+    while (Money <= 0);
+    return 0;    
 }
