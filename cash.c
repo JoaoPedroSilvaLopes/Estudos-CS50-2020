15db288225bfc08734593224793ab96a0ea2c7fb
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-08 20:05:40 +0000

    automated commit by check50 [check50=True]

--- cash.c
+++ cash.c
@@ -5,42 +5,34 @@
 
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
+
 
 int main(void)
 {
-   float Money;
-   float Moedas;
-   int Y;
-   int Y1;
-   int Y2;
-   int Y3;
-   float Money1;
-   float Money2;
-   float Money3;
+float Money;
+int QuantMoedas;
+int QuantMoedas1;
+int QuantMoedas2;
+int QuantMoedas3;
    
    do
    {
-       
-       
       Money = get_float("Digite o saldo: ");
       
-      Y = Money/0.25;
+      QuantMoedas = Money/0.25;
+      printf("%d\n\n", QuantMoedas);
+
+      QuantMoedas1 = (Money - (QuantMoedas*0.25))/0.10;
+      printf("%d\n\n", QuantMoedas1);
+
+      QuantMoedas2 = (Money - (QuantMoedas*0.25) - (QuantMoedas1*0.10))/0.05;
+      printf("%d\n\n", QuantMoedas2);
       
-      Money1 = Money - (Y*0.25);
-      
-      Y1 = Money1/0.10;
-      
-      Money2 = Money1 - (Y1*0.10);
-      
-      Y2 = Money2/0.05;
-      
-      Money3 = Money2 - (Y2*0.05);
-      
-      Y3 = Money3/0.01;
-      
-      Moedas = Y + Y1 + Y2 + Y3;
-      printf("%f\n", Moedas);
-      
+      QuantMoedas3 = (Money - (QuantMoedas*0.25) - (QuantMoedas1*0.10) - (QuantMoedas2*0.05))/0.01;
+      printf("%d\n\n", QuantMoedas3);
+
+
    }
    while(Money <= 0);
     
