3e0052dfe842b8eb01c078af53b440407d0027a8
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-08 22:05:57 +0000

    automated commit by check50 [check50=True]

--- cash.c
+++ cash.c
@@ -10,34 +10,34 @@
 
 int main(void)
 {
-float Money;
-float M;
-float N;
-float M1;
-float N1;
-float M2;
-float N2;
-float M3;
-float N3;
-float M4;
-float N4;
-float M5;
-float N5;
-float M6;
-float N6;
-float M7;
-float N7;
-float M8;
-float N8;
+double Money;
+double M;
+double N;
+double M1;
+double N1;
+double M2;
+double N2;
+double M3;
+double N3;
+double M4;
+double N4;
+double M5;
+double N5;
+double M6;
+double N6;
+double M7;
+double N7;
+double M8;
+double N8;
 int QuantMoedas;
 int QuantMoedas1;
 int QuantMoedas2;
-int QuantMoedas3;
-int MoedasTotal;
+float QuantMoedas3;
+float MoedasTotal;
    
    do
    {
-      Money = get_float("Digite o saldo: ");
+      Money = get_double("Digite o saldo: ");
       
       M = round(Money*100);
       N = M/100;
@@ -50,32 +50,53 @@ int MoedasTotal;
       M8 = round(0.01*100);
       N8 = M8/100;
       // ------------------------------
-      QuantMoedas = floor(N/N5);
+      QuantMoedas = (N/N5);
       M1 = round(QuantMoedas*100);
-      printf("%f\n", M1);
+      printf("%lf\n", M1);
       N1 = M1/100;
-      printf("%f\n\n", N1);
+      printf("%lf\n\n", N1);
+      
+      
+      
+      
+      
       // ------------------------------
-      QuantMoedas1 = floor((N - (N1*N5))/N6);
+      QuantMoedas1 = ((N - (N1*N5))/N6);
       M2 = round(QuantMoedas1*100);
-      printf("%f\n", M2);
+      printf("%lf\n", M2);
       N2 = M2/100;
-      printf("%f\n\n", N2);
+      printf("%lf\n\n", N2);
+      
+      
+      
+      
+      
+      
       // ------------------------------
-      QuantMoedas2 = floor((N - (N1*N5) - (N2*N6))/N7);
+      QuantMoedas2 = ((N - (N1*N5) - (N2*N6))/N7);
       M3 = round(QuantMoedas2*100);
-      printf("%f\n", M3);
+      printf("%lf\n", M3);
       N3 = M3/100;
-      printf("%F\n\n", N3);
+      printf("%lf\n\n", N3);
+      
+      
+      
+      
+      
       // ------------------------------
-      QuantMoedas3 = floor((N - (N1*N5) - (N2*N6) - (N3*N7))/N8);
-      M4 = round(QuantMoedas3*100);
-      printf("%f\n", M4);
+      QuantMoedas3 = ((N - (N1*N5) - (N2*N6) - (N3*N7))/N8);
+      M4 = (QuantMoedas3*100);
+      printf("%lf\n", M4);
       N4 = M4/100;
-      printf("%f\n\n", N4);
+      printf("%lf\n\n", N4);
+      
+      
+      
+      
+      
       // ------------------------------
       MoedasTotal = N1 + N2 + N3 + N4;
-      printf("%d\n", MoedasTotal);
+      printf("%f\n", MoedasTotal);
    }
    while(Money <= 0);
     
