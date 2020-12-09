c454f2319b723c44001cd4a7512fb43d94caeeb0
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-09 00:02:03 +0000

    automated commit by check50 [check50=True]

--- cash.c
+++ cash.c
@@ -10,28 +10,25 @@
 int main(void)
 {
     float Money;
-    int QuantMoedas;
-    int QuantMoedas1;
-    int QuantMoedas2;
-    int QuantMoedas3;
+    int QuantMoedas[4];
     int MoedasTotal;
     do
     {
         Money = get_float("Digite o saldo: ");
         // ------------------------------
-        QuantMoedas = round((Money / 0.25) * 100) / 100;
-        printf("Será preciso %d moeda(as) de 25 centavos.\n\n", QuantMoedas);
+        QuantMoedas[0] = round((Money / 0.25) * 100) / 100;
+        printf("Será preciso %d moeda(as) de 25 centavos.\n\n", QuantMoedas[0]);
         // ------------------------------
-        QuantMoedas1 = round(((Money - QuantMoedas * 0.25) / 0.10) * 100) / 100;
-        printf("Será preciso %d moeda(as) de 10 centavos.\n\n", QuantMoedas1);
+        QuantMoedas[1] = round(((Money - QuantMoedas[0] * 0.25) / 0.10) * 100) / 100;
+        printf("Será preciso %d moeda(as) de 10 centavos.\n\n", QuantMoedas[1]);
         // ------------------------------
-        QuantMoedas2 = round(((Money - QuantMoedas * 0.25 - QuantMoedas1 * 0.10) / 0.05) * 100) / 100;
-        printf("Será preciso %d moeda(as) de 5 centavos.\n\n", QuantMoedas2);
+        QuantMoedas[2] = round(((Money - QuantMoedas[0] * 0.25 - QuantMoedas[1] * 0.10) / 0.05) * 100) / 100;
+        printf("Será preciso %d moeda(as) de 5 centavos.\n\n", QuantMoedas[2]);
         // ------------------------------
-        QuantMoedas3 = round(((Money - QuantMoedas * 0.25 - QuantMoedas1 * 0.10 - QuantMoedas2 * 0.05) / 0.01) * 100) / 100;
-        printf("Será preciso %d moeda(as) de 1 centavo.\n\n", QuantMoedas3);
+        QuantMoedas[3] = round(((Money - QuantMoedas[0] * 0.25 - QuantMoedas[1] * 0.10 - QuantMoedas[2] * 0.05) / 0.01) * 100) / 100;
+        printf("Será preciso %d moeda(as) de 1 centavo.\n\n", QuantMoedas[3]);
         // ------------------------------
-        MoedasTotal = QuantMoedas + QuantMoedas1 + QuantMoedas2 + QuantMoedas3;
+        MoedasTotal = QuantMoedas[0] + QuantMoedas[1] + QuantMoedas[2] + QuantMoedas[3];
         printf("%d\n", MoedasTotal);
     }
     while (Money <= 0);
