dd8977dd3cd07d93f4a92ab9dd3613fbeda6c06e
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-09 00:23:59 +0000

    automated commit by check50 [check50=True]

---
+++ credit.c
@@ -0,0 +1,22 @@
+//MATRÍCULA: 20201045050643
+//NOME: João Pedro Silva Lopes
+//USUÁRIO: JoãoPedroSilvaLopes
+//EMAIL INSTITUCIONAL: joao.pedro.silva06@aluno.ifce.edu.br
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+   long Número = get_long("Insira o número do cartão: "); 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+return 0;    
+}
