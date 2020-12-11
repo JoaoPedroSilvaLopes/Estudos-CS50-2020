6ac49e6729b2d5306b0c29db59736feb74abff26
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-11 22:58:54 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -7,7 +7,7 @@
 #include <math.h>
 
 int i = 0;
-int NS[16];
+int NS[17];
 long Resto;
 int Resto1;
 int Soma;
@@ -21,12 +21,16 @@ int main(void)
         for (int z = 14; z >= 0; z--)
         {
             NS[i] = NC / pow(10, z);
+            printf("%i\n", NS[i]);
             Resto = NC % (long) pow(10, z);
+            printf("%li\n", Resto);
             NC = Resto;
             i++;
         }
-        Soma = ((NS[0] + NS[2] + NS[4] + NS[6] + NS[8] + NS[10] + NS[12] + NS[14]) * 2) 
-               + NS[1] + NS[3] + NS[5] + NS[7] + NS[9] + NS[11] + NS[13] + NS[15];
+        Soma = ((NS[1] + NS[3] + NS[5] + NS[7] + NS[9] + NS[11] + NS[13]) * 2) 
+               + NS[0] + NS[2] + NS[4] + NS[6] + NS[8] + NS[10] + NS[12] + NS[14];
+               
+
         printf("%i\n", Soma);
         Resto1 = Soma % 10;
         if (Resto1 == 0)
@@ -44,11 +48,13 @@ int main(void)
         for (int z = 15; z >= 0; z--)
         {
             NS[i] = NC / pow(10, z);
+            printf("%i\n", NS[i]);
             Resto = NC % (long) pow(10, z);
+            printf("%li\n", Resto);
             NC = Resto;
             i++;
         }
-        Soma = ((NS[0] + NS[2] + NS[4] + NS[6] + NS[8] + NS[10] + NS[12] + NS[14]) * 2) 
+        Soma = (NS[0]*2 + NS[2]*2 + NS[4]*2 + NS[6]*2 + NS[8]*2 + NS[10]*2 + NS[12]*2 + NS[14]*2) 
                + NS[1] + NS[3] + NS[5] + NS[7] + NS[9] + NS[11] + NS[13] + NS[15];
         printf("%i\n", Soma);
         Resto1 = Soma % 10;
@@ -61,18 +67,20 @@ int main(void)
             printf("INVALID\n");
         }
     }
-    else if ((NC >= 4 * pow(10, 12) && NC < 5 * pow(10, 12)) || (NC >= 4 * pow(10, 15) && NC < 5 * pow(10, 15)))
+    else if ((NC >= 4 * (long) pow(10, 12) && NC < 5 * (long) pow(10, 12)) || (NC >= 4 * (long) pow(10, 15) && NC < 5 * (long) pow(10, 15)))
     {
         if (NC < 1 * pow(10, 15))
         {
             for (int z = 15; z >= 0; z--)
             {
-                NS[i] = NC / pow(10, z);
+                NS[i] = NC / (long) pow(10, z);
+                printf("%i\n", NS[i]);
                 Resto = NC % (long) pow(10, z);
+                printf("%li\n", Resto);
                 NC = Resto;
                 i++;
             }
-            Soma = ((NS[0] + NS[2] + NS[4] + NS[6] + NS[8] + NS[10] + NS[12] + NS[14]) * 2) 
+            Soma = (NS[0]*2 + NS[2]*2 + NS[4]*2 + NS[6]*2 + NS[8]*2 + NS[10]*2 + NS[12]*2 + NS[14]*2) 
                    + NS[1] + NS[3] + NS[5] + NS[7] + NS[9] + NS[11] + NS[13] + NS[15];
             printf("%i\n", Soma);
             Resto1 = Soma % 10;
@@ -89,12 +97,14 @@ int main(void)
         {
             for (int z = 12; z >= 0; z--)
             {
-                NS[i] = NC / pow(10, z);
+                NS[i] = NC / (long) pow(10, z);
+                printf("%i\n", NS[i]);
                 Resto = NC % (long) pow(10, z);
+                printf("%li\n", Resto);
                 NC = Resto;
                 i++;
             }
-            Soma = ((NS[0] + NS[2] + NS[4] + NS[6] + NS[8] + NS[10] + NS[12]) * 2) 
+            Soma = (NS[0]*2 + NS[2]*2 + NS[4]*2 + NS[6]*2 + NS[8]*2 + NS[10]*2 + NS[12]*2) 
                    + NS[1] + NS[3] + NS[5] + NS[7] + NS[9] + NS[11];
             printf("%i\n", Soma);
             Resto1 = Soma % 10;
