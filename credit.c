68904bade38e776162f45f69c08f8c2698540889
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-11 17:56:16 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -21,14 +21,11 @@ int main(void)
         for(int z = 14; z >= 0; z--)
         {
             NS[0] = NC/pow(10,z);
-            printf("%i\n", NS[0]);
             Resto = NC % (long) pow(10,z);
-            printf("%li\n", Resto);
             NC = Resto;
             i++;
         }
         Soma = (NS[0]*2 + NS[2]*2 + NS[4]*2 + NS[6]*2 + NS[8]*2 + NS[10]*2 + NS[12]*2 + NS[14]*2) + NS[1] + NS[3] +  NS[5] + NS[7] +  NS[9] + NS[11] +  NS[13] + NS[15];
-        printf("%i\n", Soma);
         Resto1 = Soma % 10;
         if (Resto1 == 0)
         {
