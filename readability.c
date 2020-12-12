95e8410ffbab9d11a419886c202a6a496fd6d950
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-12 04:23:47 +0000

    automated commit by check50 [check50=True]

---
+++ readability.c
@@ -0,0 +1,25 @@
+//MATRÍCULA: 20201045050643
+//NOME: João Pedro Silva Lopes
+//USUÁRIO: JoãoPedroSilvaLopes
+//EMAIL INSTITUCIONAL: joao.pedro.silva06@aluno.ifce.edu.br
+
+#include <stdio.h> 
+#include <cs50.h> 
+#include <math.h>
+#include <ctype.h>
+
+int main(void)
+{
+    
+    
+    
+    
+    return 0;
+}
+
+
+/*
+$$I = 0.0588 * L - 0.296 * S - 15.8$$
+L = número médio de letras a cada 100 palavras no texto.
+S = média do número de sentenças a cada 100 palavras no texto.
+*/
