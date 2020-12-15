bda7988a769ac855b6ac63d9fde907a964b6f1ff
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-15 01:20:40 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -2,24 +2,64 @@
 //NOME: João Pedro Silva Lopes
 //USUÁRIO: JoãoPedroSilvaLopes
 //EMAIL INSTITUCIONAL: joao.pedro.silva06@aluno.ifce.edu.br
-
 #include <stdio.h> 
 #include <cs50.h> 
 #include <math.h>
 #include <ctype.h>
+#include <string.h>
+
+int Tamanho, Palavras, L, S, I;
+char espaço[] = " ";
+char Texto[2000];
+int Espaços, Letras, Sentenças = 0;
 
 int main(void)
 {
+    printf("Insira a frase: \n");
+    scanf("%s\n", Texto);
+    fgets(Texto,1000,stdin);
+    Tamanho = strlen(Texto);
+    printf("%i\n", Tamanho);
+
+    for (int x = 0; x <= Tamanho; x++)
+    {
+        if (Texto[x] == ' ')
+        {
+            Espaços++;
+        }
+        else if (Texto[x] == '.' || Texto[x] == '!' || Texto[x] == '?')
+        {
+            Sentenças++;
+        }
+        else
+        {
+
+            Letras++;
+        }
+    }
+    printf("%i\n", Espaços);
+    printf("%i\n", Sentenças);
+    printf("%i\n", Letras);
     
+    Palavras = Espaços + 1;
     
+    L = (Letras * 100) / Palavras;
+    S = (Sentenças * 100) / Palavras;
     
+    I = 0.0588 * L - 0.296 * S - 15.8;
+    printf("Grade %i\n", I);
     
     return 0;
 }
-
 
 /*
 $$I = 0.0588 * L - 0.296 * S - 15.8$$
 L = número médio de letras a cada 100 palavras no texto.
 S = média do número de sentenças a cada 100 palavras no texto.
+isspace (verifica um espaço em branco)
+ispunct (verifica um ponto)
+islower (verifica um caractere minúsculo)
+isupper (verifica um caractere maiúculo)
+isalpha (verifica se o caractere é uma letra do alfabeto)
+strlen (verifica o tamanho de uma string)
 */
