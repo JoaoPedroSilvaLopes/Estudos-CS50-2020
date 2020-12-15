636634c622ae69a47aa2d1af5795277c1435989a
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-15 19:30:06 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -7,47 +7,55 @@
 #include <math.h> //Utilizar Round
 #include <string.h> //Utilizar strlen();
 
-int Tamanho, I, Palavras, Espaços = 0, Letras = 0, Sentenças = 0; //Declaração de variáveis inteiras
-float  L, S;
+int Tamanho, I, Espaços = 0, Letras = 0, Sentenças = 0; //Declaração de variáveis inteiras.
+float  L, S, Palavras; //Declaração de variáveis decimais.
 
 int main(void)
 {
-    string Texto = get_string("Insira o texto: \n");
-    Tamanho = strlen(Texto);
-    for (int x = 0; x < Tamanho; x++)
+    string Texto = get_string("Insira o texto: \n"); //Get_string("") para inserir o texto a ser analisado.
+    Tamanho = strlen(Texto); //Tamanho é a contagem de caracteres do texto, que foi contado com strlen();.
+    for (int x = 0; x < Tamanho; x++) //For para contar os caracteres, dividindo-os em letras, espaços e sentenças.
     {
-        if (Texto[x] == ' ')
+        //If caso o caractere seja um espaço.
+        if (Texto[x] == ' ') 
         {
-            Espaços++;
+            Espaços++; //Soma +1 a quantidade de espaços.
         }
-        else if (Texto[x] == '.' || Texto[x] == '!' || Texto[x] == '?')
+        //Else if caso o caractere seja uma das pontuações.
+        else if (Texto[x] == '.' || Texto[x] == '!' || Texto[x] == '?') 
         {
-            Sentenças++;
+            Sentenças++; //Soma +1 a quantidade de senteças.
         }
-        else if ((Texto[x] >= 'a' && Texto[x] <= 'z') || (Texto[x] >= 'A' && Texto[x] <= 'Z'))
+        //Else if caso o caractere uma letra maiúscula ou minúscula.
+        else if ((Texto[x] >= 'a' && Texto[x] <= 'z') || (Texto[x] >= 'A' && Texto[x] <= 'Z')) 
         {
-            Letras++;
+            Letras++; //Soma +1 a quantidade de letras.
         }
+        //Else para caso o caractere não seja nenhum dos casos acima.
         else
         {
-
+            //Não deve acontecer nada nesse caso.
         }
     }
-    Palavras = Espaços + 1;
+    //Números de palavras é o números de espaços +1, depois do último espaço certamente terá uma palavra.
+    Palavras = Espaços + 1; 
+    // L é (letras * 100) / palavras, palavras é float para o resultado não ser arredondado para inteiro.
     L = (Letras * 100) / Palavras;
+    // S é (Sentenças * 100) / palavras, palavras é float para o resultado não ser arredondado para inteiro.
     S = (Sentenças * 100) / Palavras;
+    // I é um número inteiro com o auxílio de round para dar o inteiro mais próximo do o arrondamento ser mais preciso.
     I = round((0.0588 * L) - (0.296 * S) - 15.8);
-    if (I >= 16)
+    if (I >= 16) //If caso I seja maior ou igual a 16 como foi pedido.
     {
-        printf("Grade 16+\n");
+        printf("Grade 16+\n"); //Caso dê maior ou igual a 16 imprimirá Grade 16+.
     }
-    else if (I < 1)
+    else if (I < 1) //Else if caso I seja menor que 1 como foi pedido.
     {
-        printf("Before Grade 1\n");
+        printf("Before Grade 1\n"); //Caso dê menor que 1 imprimirá Before grade 1.
     }
-    else
+    else //Else caso o I não seja nenhum outro caso.
     {
-        printf("Grade %i\n", I);
+        printf("Grade %i\n", I); //Irá imprimir o próprio resultado da fórmula.
     }
     
     return 0;
