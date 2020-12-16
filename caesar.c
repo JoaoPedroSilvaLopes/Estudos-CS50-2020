9b11e70576657ad0433d86006ed3258984ff4d17
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-16 03:45:30 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -5,20 +5,41 @@
 #include <stdio.h> 
 #include <cs50.h> 
 #include <string.h>
+#include <math.h>
 
+int z;
 int Tamanho;
-int main(void)
+int main()
 {
     int x = get_int("");
+    if ( x >= 1)
+    {
+        z = 0;
+    }
+    else
+    {
+        z = 1;
+    }
+
+
+    return z;
+}
+
+
+/*int x = get_int("");
     string Frase = get_string("");
     Tamanho = strlen(Frase);
     
     for(int y = 0; y < Tamanho; y++)
     {
-        Frase[y] = Frase[y] + x;
+        if((Frase[y] >= 'a' && Frase[y] <= 'z') || (Frase[y] >= 'A' && Frase[y] <= 'Z'))
+        {
+            Frase[y] = Frase[y] + x;
+        }
+        else
+        {
+            
+        }
     }
     printf("%s\n", Frase);
-
-    return 0;
-}
-
+*/
