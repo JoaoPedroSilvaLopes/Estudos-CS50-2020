d60d1dfceb2304040fc5d7539693cf28d6534f2b
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-18 23:11:09 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -15,8 +15,8 @@ int Retorno;
 int main(int argc, string argv[])
 {
     Key = atoi(argv[1]);
-    //printf("%d\n", Key);
-    if (argc == 2 && (Key > 0))
+    printf("%d\n", Key);
+    if (argc == 2)
     {
         string Frase = get_string("plaintext: ");
         Tamanho = strlen(Frase);
