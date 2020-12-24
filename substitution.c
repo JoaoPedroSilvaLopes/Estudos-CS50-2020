66fe828699fc3fc5873e6401d6c581eae457e3a2
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-24 07:52:05 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -57,7 +57,7 @@ int main(int argc, string argv[])
                 {
                     if (Letras == tolower(Alfabeto[j]))
                     {
-                            printf("%c", tolower(argv[1][j]));
+                        printf("%c\n", tolower(argv[1][j]));
                     }
                 }
             }
@@ -67,7 +67,7 @@ int main(int argc, string argv[])
                 {
                     if (Letras == toupper(Alfabeto[j]))
                     {
-                        printf("%c", toupper(argv[1][j]));
+                        printf("%c\n", toupper(argv[1][j]));
                     }
                 }
             }
