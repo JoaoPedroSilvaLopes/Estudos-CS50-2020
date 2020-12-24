4ab37d0ca81297bd53c8986faabcf85fee176156
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-24 03:00:08 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -50,7 +50,7 @@ int main(int argc, string argv[])
 void Substituir(string chave)
 {
     string x = get_string("plaintext: ");
-    printf("ciphertext: ""\n");
+    printf("ciphertext: %s\n");
     
     for (int i = 0; i < strlen(x); i++)
     {
