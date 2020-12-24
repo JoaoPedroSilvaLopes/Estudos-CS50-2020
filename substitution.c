60643ba4825866a50e810154510eb8c1e6b91507
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-24 01:26:41 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -7,21 +7,27 @@
 #include <string.h>
 #include <ctype.h>
 
-int const X = 26;
 const string Letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-int Número;
+int Tamanho;
 
 int main(int argc, string argv[])
 {
-    if (argc != 2)
+    Tamanho = strlen(argv[1]);
+    //printf("%d\n", Tamanho);
+    
+    if (argc != 2 || Tamanho != 26)
     {
         printf("Usage: %s key\n", argv[0]);
         return 1;
     }
     
-    int Caracteres [X];
-    Número = strlen(argv[1]);
     
+    
+    
+    
+    /*int Caracteres [X];
+    Número = strlen(argv[1]);
+
     for (int x = 0; x < Número; x++)
     if (!(isalpha(argv[1][x])))
     {
@@ -32,16 +38,6 @@ int main(int argc, string argv[])
     {
         argv[1][x] = toupper(argv[1][x]);
     }
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
     string Texto = get_string("plaintext: ");
     int Y = strlen(Texto);
     char TextoC[Y + 1];
@@ -66,7 +62,7 @@ int main(int argc, string argv[])
         }
     }
 
-    printf("ciphertext: %s\n", TextoC);
+    printf("ciphertext: %s\n", TextoC);*/
     
     return 0;
 }
