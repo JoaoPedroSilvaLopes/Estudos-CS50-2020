cb4b4dff868488631564763f323203ee968a9340
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-25 20:58:16 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -48,7 +48,7 @@ int main(void)
                 D[j] = atol(&NC[j]) / pow(10, strlen(&NC[j]) - 1);
             }
         }
-        Soma = D[0] + D[1] + D[2] + D[3] + D[4] + D[5] + D[6] + D[7] + D[8] + D[9] + D[10] + D[11] + D[12] + D[13] + D[14];
+        Soma = D[0] + D[1] + D[2] + D[3] + D[4] + D[5] + D[6] + D[7] + D[8] + D[9] + D[10] + D[11] + D[12] + D[13] + D[14] + D[15];
         if (Soma % 10 == 0)
         {
             printf("CARTÃO\n");
