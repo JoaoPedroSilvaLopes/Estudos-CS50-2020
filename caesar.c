9a2cce3e0134ba8ddb1d45d860ad8cfdfb89265e
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-25 05:46:07 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -11,51 +11,17 @@ int Key;
 
 int main(int argc, string argv[])
 {
-    if (argc == 2)
+    for (int i = 0; i < strlen(argv[1]); i++)
     {
-        Key = strlen(argv[1]);
-        string Frase = get_string("plaintext: ");
-        
-        for (int i = 0; i < strlen(Frase); i++)
+        if (isdigit(argv[1][i]))
         {
-            if (isalpha(Frase[i]))
-            {
-                /*Key = strlen(argv[1]);
-                Key = Key - ('z' - Frase[i]);
-                if (islower(Frase[i]))
-                {
-                    Frase[i] = Frase[i] + strlen(argv[1]);
-                }
-                else if
-                {
-                    Frase[i] = ('a' - 1) + (Key % 26);
-                }
-                else if (isupper(Frase[i]))
-                {
-                    Key = strlen(argv[1]);
-                    Key = Key - ('Z' - Frase[i]);
-                    if (Key < 0)
-                    {
-                        Frase[i] = Frase[i] + strlen(argv[1]);
-                    }
-                }
-                else
-                {
-                    Frase[i] = ('A' - 1) + (Key % 26);
-                }*/
-            }
-            else
-            {
-                printf("Usage: %s\n", argv[0]);
-                return 1;
-            }
+            printf("OK\n");
         }
-        printf("ciphertext: %s\n", Frase);
-    }
-    else
-    {
-        printf("Usage: %s\n", argv[0]);
-        return 1;
+        else
+        {
+            printf("Usage: %s\n", argv[0]);
+            return 1;
+        }
     }
     return 0;
 }
