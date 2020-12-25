589c85338653695e35f1732adb05a7edea25b338
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2020-12-25 21:10:06 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -73,9 +73,9 @@ int main(void)
             }
         }
         Soma = D[0] + D[1] + D[2] + D[3] + D[4] + D[5] + D[6] + D[7] + D[8] + D[9] + D[10] + D[11] + D[12] + D[13] + D[14] + D[15];
-        if (Soma % 10 == 0)
+        if (Soma % 10 == 0 && ((NC[0] == '3' && NC[1] == '7') || (NC[0] == '3' && NC[1] == '7')))
         {
-            printf("CARTÃO\n");
+            printf("AMEX\n");
         }
         else
         {
