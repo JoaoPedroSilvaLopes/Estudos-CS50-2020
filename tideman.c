5d69926cdf08171b068063a36c779f121a9ac78d
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2021-03-22 00:44:07 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -133,11 +133,11 @@ void record_preferences(int ranks[])
                         n = y;
                     }
                 }
-                
                 if (m < n)
                 {
                     preferences[i][j]++;
                 }
+                printf("%i\n", preferences[i][j]);
             }
         }
     }
@@ -180,11 +180,11 @@ void add_pairs(void)
                     }
                     else if (preferences[i][j] < preferences[j][i])
                     {
-                        pairs[pair_count].winner = preferences[j][i];
-                        pairs[pair_count].loser = preferences[i][j];
-                        preferences[i][j] = 0;
-                        preferences[j][i] = 0;
-                        pair_count++;    
+                        //pairs[pair_count].winner = preferences[j][i];
+                        //pairs[pair_count].loser = preferences[i][j];
+                        //preferences[i][j] = 0;
+                        //preferences[j][i] = 0;
+                        //pair_count++;    
                     }
                 }
             }
@@ -205,7 +205,7 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    for (int i = 1; i < pair_count + 1; i++)
+    /*for (int i = 1; i < pair_count + 1; i++)
     {
         pairs[i].winner = pairs[i].winner - pairs[i].loser;  
         pairs[i].loser = 0;
@@ -213,7 +213,7 @@ void sort_pairs(void)
     
     printf("%i - %i\n\n", pairs[0].winner, pairs[0].loser);
     printf("%i - %i\n\n", pairs[1].winner, pairs[1].loser);
-    printf("%i - %i\n\n", pairs[2].winner, pairs[2].loser);
+    printf("%i - %i\n\n", pairs[2].winner, pairs[2].loser);*/
 
     return;
 }
