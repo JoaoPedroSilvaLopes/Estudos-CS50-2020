967f1583f6ad64bcc899c85405081363b722cacf
JoaoPedroSilvaLopes <JoaoPedroSilvaLopes@users.noreply.github.com>
2021-03-25 05:04:18 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -213,14 +213,14 @@ void lock_pairs(void)
 
     for (int i = 0; i < pair_count; i++)
     {
-        //if (pairs[i].loser == pairs[0].winner)
-        //{
+        if (pairs[i].loser == pairs[0].winner)
+        {
             locked[pairs[i].winner][pairs[i].loser] = false;
-        //}
-        //else
-        //{
-            //locked[pairs[i].winner][pairs[i].loser] = true;
-        //}
+        }
+        else
+        {
+            locked[pairs[i].winner][pairs[i].loser] = true;
+        }
     }
 
     /*for (int i = 0; i < pair_count; i++)
